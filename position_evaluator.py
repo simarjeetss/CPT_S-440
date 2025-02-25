f4560fa1abeac6e8f1cd04046b65efe0e4f42406
“simarjeetss” <simarjeetss529@gmail.com>
2025-02-25 15:35:00 -0800

    added intial setup for position evaluator

---
+++ position_evaluator.py
@@ -0,0 +1,114 @@
+import numpy as np
+
+1
+class PositionEvaluator:
+    """A chess position evaluator that estimates the strength of a position."""
+
+    def __init__(self):
+        # Material values (in centipawns)
+        self.piece_values = {
+            'p': 100,  # pawn
+            'n': 320,  # knight
+            'b': 330,  # bishop
+            'r': 500,  # rook
+            'q': 900,  # queen
+            'k': 20000  # king (not really used in evaluation, but for completeness)
+        }
+
+        # Piece-square tables for positional bonuses
+        # Each table is an 8x8 grid representing bonus/penalty values for piece positions
+        # From white's perspective (values are mirrored for black)
+
+        # Pawn position table
+        self.pawn_table = np.array([
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [50, 50, 50, 50, 50, 50, 50, 50],
+            [10, 10, 20, 30, 30, 20, 10, 10],
+            [5, 5, 10, 25, 25, 10, 5, 5],
+            [0, 0, 0, 20, 20, 0, 0, 0],
+            [5, -5, -10, 0, 0, -10, -5, 5],
+            [5, 10, 10, -20, -20, 10, 10, 5],
+            [0, 0, 0, 0, 0, 0, 0, 0]
+        ])
+
+        # Knight position table
+        self.knight_table = np.array([
+            [-50, -40, -30, -30, -30, -30, -40, -50],
+            [-40, -20, 0, 0, 0, 0, -20, -40],
+            [-30, 0, 10, 15, 15, 10, 0, -30],
+            [-30, 5, 15, 20, 20, 15, 5, -30],
+            [-30, 0, 15, 20, 20, 15, 0, -30],
+            [-30, 5, 10, 15, 15, 10, 5, -30],
+            [-40, -20, 0, 5, 5, 0, -20, -40],
+            [-50, -40, -30, -30, -30, -30, -40, -50]
+        ])
+
+        # Bishop position table
+        self.bishop_table = np.array([
+            [-20, -10, -10, -10, -10, -10, -10, -20],
+            [-10, 0, 0, 0, 0, 0, 0, -10],
+            [-10, 0, 10, 10, 10, 10, 0, -10],
+            [-10, 5, 5, 10, 10, 5, 5, -10],
+            [-10, 0, 5, 10, 10, 5, 0, -10],
+            [-10, 5, 5, 5, 5, 5, 5, -10],
+            [-10, 0, 5, 0, 0, 5, 0, -10],
+            [-20, -10, -10, -10, -10, -10, -10, -20]
+        ])
+
+        # Rook position table
+        self.rook_table = np.array([
+            [0, 0, 0, 5, 5, 0, 0, 0],
+            [-5, 0, 0, 0, 0, 0, 0, -5],
+            [-5, 0, 0, 0, 0, 0, 0, -5],
+            [-5, 0, 0, 0, 0, 0, 0, -5],
+            [-5, 0, 0, 0, 0, 0, 0, -5],
+            [-5, 0, 0, 0, 0, 0, 0, -5],
+            [5, 10, 10, 10, 10, 10, 10, 5],
+            [0, 0, 0, 0, 0, 0, 0, 0]
+        ])
+
+        # Queen position table
+        self.queen_table = np.array([
+            [-20, -10, -10, -5, -5, -10, -10, -20],
+            [-10, 0, 0, 0, 0, 0, 0, -10],
+            [-10, 0, 5, 5, 5, 5, 0, -10],
+            [-5, 0, 5, 5, 5, 5, 0, -5],
+            [0, 0, 5, 5, 5, 5, 0, -5],
+            [-10, 5, 5, 5, 5, 5, 0, -10],
+            [-10, 0, 5, 0, 0, 0, 0, -10],
+            [-20, -10, -10, -5, -5, -10, -10, -20]
+        ])
+
+        # King middle game position table (protects the king, encourages castling)
+        self.king_middle_table = np.array([
+            [-30, -40, -40, -50, -50, -40, -40, -30],
+            [-30, -40, -40, -50, -50, -40, -40, -30],
+            [-30, -40, -40, -50, -50, -40, -40, -30],
+            [-30, -40, -40, -50, -50, -40, -40, -30],
+            [-20, -30, -30, -40, -40, -30, -30, -20],
+            [-10, -20, -20, -20, -20, -20, -20, -10],
+            [20, 20, 0, 0, 0, 0, 20, 20],
+            [20, 30, 10, 0, 0, 10, 30, 20]
+        ])
+
+        # King end game position table (king becomes active in endgame)
+        self.king_end_table = np.array([
+            [-50, -40, -30, -20, -20, -30, -40, -50],
+            [-30, -20, -10, 0, 0, -10, -20, -30],
+            [-30, -10, 20, 30, 30, 20, -10, -30],
+            [-30, -10, 30, 40, 40, 30, -10, -30],
+            [-30, -10, 30, 40, 40, 30, -10, -30],
+            [-30, -10, 20, 30, 30, 20, -10, -30],
+            [-30, -30, 0, 0, 0, 0, -30, -30],
+            [-50, -30, -30, -30, -30, -30, -30, -50]
+        ])
+
+        # Create a mapping for piece tables lookup
+        self.piece_to_table = {
+            'p': self.pawn_table,
+            'n': self.knight_table,
+            'b': self.bishop_table,
+            'r': self.rook_table,
+            'q': self.queen_table,
+            'k': self.king_middle_table  # Default to middle game
+        }
